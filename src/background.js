3e55b420350c7d7334107e41a2aa83add6ef3d60
x86 <1960867973@qq.com>
2021-12-31 12:01:44 +0800

    粘贴发送文件，只支持 windows 和 mac

--- src/background.js
+++ src/background.js
@@ -25,7 +25,6 @@ import pkg from '../package.json';
 import Badge from 'electron-windows-badge';
 import {createProtocol} from "vue-cli-plugin-electron-builder/lib";
 import IPCRendererEventType from "./ipcRendererEventType";
-import clipboardEx from 'electron-clipboard-ex'
 import nodePath from 'path'
 
 // Scheme must be registered before the app is ready
@@ -645,19 +644,23 @@ const createMainWindow = async () => {
 
             fs.writeFileSync(filename, image.toPNG());
         } else {
-            const filePaths = clipboardEx.readFilePaths();
-            if (filePaths){
-                args = {
-                    hasFile: true,
-                    files: [],
-                };
-                filePaths.forEach(path => {
-                    args.files.push({
-                        path: path,
-                        name: nodePath.basename(path),
-                        size: fs.statSync(path).size,
+            const clipboardEx = require('electron-clipboard-ex')
+            // only support windows and mac
+            if(clipboardEx){
+                const filePaths = clipboardEx.readFilePaths();
+                if (filePaths){
+                    args = {
+                        hasFile: true,
+                        files: [],
+                    };
+                    filePaths.forEach(path => {
+                        args.files.push({
+                            path: path,
+                            name: nodePath.basename(path),
+                            size: fs.statSync(path).size,
+                        })
                     })
-                })
+                }
             }
         }
 
