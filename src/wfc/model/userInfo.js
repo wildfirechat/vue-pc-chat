7aac06f8a5cbd2ef4be3039cf758c5755fc31300
x86 <1960867973@qq.com>
2022-01-24 15:12:22 +0800

    更新会议参与者信息. fix #137

--- src/wfc/model/userInfo.js
+++ src/wfc/model/userInfo.js
@@ -4,18 +4,18 @@
 
 /*
 {
-    "uid": "uiuJuJcc", 
-    "name": "13888888888", 
-    "displayName": "imndx", 
-    "gender": 0, 
-    "portrait": "https://avatars.io/gravatar/uiuJuJcc", 
-    "mobile": "13888888888", 
-    "email": "", 
-    "address": "", 
-    "company": "", 
-    "social": "", 
-    "extra": "", 
-    "type": -472579968, 
+    "uid": "uiuJuJcc",
+    "name": "13888888888",
+    "displayName": "imndx",
+    "gender": 0,
+    "portrait": "https://avatars.io/gravatar/uiuJuJcc",
+    "mobile": "13888888888",
+    "email": "",
+    "address": "",
+    "company": "",
+    "social": "",
+    "extra": "",
+    "type": -472579968,
     "updateDt": 1550652404513
 }
 
@@ -36,5 +36,5 @@ export default class UserInfo {
     type = 0; //0 normal; 1 robot; 2 thing;
     //0 normal; 1 deleted;
     deleted = 0;
-    updateDt = 1550652404513;
+    updateDt = 0;
 }
