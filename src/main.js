d8766fc53697060d4d3a3d15bbcf72928c2eb599
x86 <1960867973@qq.com>
2022-07-09 21:40:41 +0800

    remove vue-i18n

--- src/main.js
+++ src/main.js
@@ -15,7 +15,6 @@ import store from "@/store";
 import visibility from 'vue-visibility-change';
 import {isElectron, remote} from "@/platform";
 import {getItem} from "./ui/util/storageHelper";
-import VueI18n from 'vue-i18n'
 import Notifications from 'vue-notification'
 import Alert from "./ui/common/Alert.js";
 import Picker from "./ui/common/Picker";
@@ -77,20 +76,19 @@ Vue.use(VModal);
 
 Vue.use(visibility);
 
-Vue.use(VueI18n)
 Vue.use(Alert)
 Vue.use(Picker)
 Vue.use(Forward)
 
-const i18n = new VueI18n({
-    // 使用localStorage存储语言状态是为了保证页面刷新之后还是保持原来选择的语言状态
-    locale: getItem('lang') ? getItem('lang') : 'zh-CN', // 定义默认语言为中文
-    messages: {
-        'zh-CN': require('@/assets/lang/zh-CN.json'),
-        'zh-TW': require('@/assets/lang/zh-TW.json'),
-        'en': require('@/assets/lang/en.json')
-    }
-})
+// const i18n = new VueI18n({
+//     // 使用localStorage存储语言状态是为了保证页面刷新之后还是保持原来选择的语言状态
+//     locale: getItem('lang') ? getItem('lang') : 'zh-CN', // 定义默认语言为中文
+//     messages: {
+//         'zh-CN': require('@/assets/lang/zh-CN.json'),
+//         'zh-TW': require('@/assets/lang/zh-TW.json'),
+//         'en': require('@/assets/lang/en.json')
+//     }
+// })
 
 Vue.use(Notifications)
 
@@ -102,10 +100,11 @@ const router = new VueRouter({
 
 Vue.prototype.$eventBus = new Vue();
 
+Vue.prototype.$t = (args) => args
+
 var vm = new Vue({
     el: '#app',
     router,
-    i18n,
     render: h => h(App),
 })
 vm.store = store.state;
