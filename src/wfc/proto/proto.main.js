85895c294c3abfaf45a3678f280000098f91dc86
x86 <1960867973@qq.com>
2022-08-25 10:53:24 +0800

    refactor

--- src/wfc/proto/proto.main.js
+++ src/wfc/proto/proto.main.js
@@ -1,7 +1,6 @@
 import {
     ipcMain,
 } from 'electron';
-import {ba} from "../../vendor/pinyin/data/dict-zi-web";
 
 let proto;
 const ASYNC_CALLBACK = 'async-callback';
@@ -17,23 +16,34 @@ export function init() {
         return proto.updateMessage(...args);
     })
 
+    // ipcMain.handle('invokeProto', (event, args) => {
+    //     console.log('invokeProto main', args);
+    //     return proto[args.methodName](...args.args);
+    // })
+
+    ipcMain.on('invokeProto', (event, args) => {
+        console.log('invokeProto main', args);
+        event.returnValue =  proto[args.methodName](...args.methodArgs);
+    })
+
     ipcMain.on('sendSavedMessage', (event, args) => {
         const {reqId, messageId, expireDuration} = args;
         proto.sendSavedMessage(messageId, expireDuration, (messageUid, timestamp) => {
             // send back to renderer window
             _asyncCallback(event, reqId, 0, true, messageUid, timestamp);
             console.log('sendSavedMessage main, sb', messageUid, timestamp);
-        }, (err) => {
-            _asyncCallback(event, reqId, 1, true, err);
+        }, (...err) => {
+            console.log('sendSavedMessage main, eb', ...err)
+            _asyncCallback(event, reqId, 1, true, ...err);
         });
     })
 
     ipcMain.on('getUploadMediaUrl', (event, args) => {
         const {reqId, fileName, mediaType, contentType} = args;
-        proto.getUploadMediaUrl(fileName, mediaType, contentType, (uploadUrl, remoteUrl, backUploadUrl, serverType) => {
+        proto.getUploadMediaUrl(fileName, mediaType, contentType, (...args) => {
             // send back to renderer window
-            _asyncCallback(event, reqId, 0, true, uploadUrl, remoteUrl, backUploadUrl, serverType);
-            console.log('getUploadMediaUrl main, sb', uploadUrl, remoteUrl, backUploadUrl, serverType);
+            _asyncCallback(event, reqId, 0, true, ...args);
+            console.log('getUploadMediaUrl main, sb', ...args);
         }, (err) => {
             _asyncCallback(event, reqId, 1, true, err);
         });
--- src/wfc/proto/proto.renderer.js
+++ src/wfc/proto/proto.renderer.js
@@ -18,17 +18,17 @@ export class ProtoRenderer {
         })
     }
 
-    getUserInfo(userId, refresh, groupId) {
-        return ipcRenderer.invoke('getUserInfo', userId, refresh, groupId);
-    }
-
-    updateMessage(messageId, messageContent){
-        return ipcRenderer.invoke('updateMessage', [messageId, messageContent]);
-    }
-
-    insertMessage(conversation, messageContent, status, notify = false, serverTime = 0) {
-        return ipcRenderer.invoke('insertMessage', conversation, messageContent, status, notify, serverTime);
-    }
+    // getUserInfo(userId, refresh, groupId) {
+    //     return ipcRenderer.invoke('getUserInfo', userId, refresh, groupId);
+    // }
+    //
+    // updateMessage(messageId, messageContent) {
+    //     return ipcRenderer.invoke('updateMessage', [messageId, messageContent]);
+    // }
+    //
+    // insertMessage(conversation, messageContent, status, notify = false, serverTime = 0) {
+    //     return ipcRenderer.invoke('insertMessage', conversation, messageContent, status, notify, serverTime);
+    // }
 
     sendSavedMessage(messageId, expireDuration, successCB, failCB) {
         let reqId = this.requestId();
@@ -45,6 +45,20 @@ export class ProtoRenderer {
     requestId() {
         this._requestId++;
         return this._requestId;
+    }
+
+    invoke(methodName, ...args) {
+        let channel = 'invokeProto';
+        // invoke 是异步调用，返回的是 Promise，对应主进程是 handle
+        // sendSync 是同步调用，返回的直接就是具体的返回值，对应主进程是 on
+        return ipcRenderer.sendSync(channel, {
+            methodName: methodName,
+            methodArgs: [...args]
+        });
+    }
+
+    invokeAsync(name, args, callbacks) {
+
     }
 
     _wrapCallback(reqId, ...callbacks) {
