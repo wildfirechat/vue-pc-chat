2804b4145728adf00fd5c141592429161df1e13c
x86 <1960867973@qq.com>
2022-09-16 12:10:17 +0800

    fix conversation.top issue

--- src/wfc/model/conversationInfo.js
+++ src/wfc/model/conversationInfo.js
@@ -23,6 +23,8 @@ export default class ConversationInfo {
 
     static protoConversationToConversationInfo(obj) {
         let conversationInfo = Object.assign(new ConversationInfo(), obj);
+        conversationInfo.top = obj.isTop;
+        delete conversationInfo.isTop;
         if (obj.conversation) {
             conversationInfo.conversation = new Conversation(obj.conversation.type, obj.conversation.target, obj.conversation.line);
         } else {
