716a1c5a974d7706ea615ba1174ba141965fbbd5
x86 <1960867973@qq.com>
2022-10-19 19:23:19 +0800

    添加私有部署提示

--- src/ui/main/setting/SettingPage.vue
+++ src/ui/main/setting/SettingPage.vue
@@ -46,6 +46,12 @@
                           :closeOnOutsideClick="true">
                 </dropdown>
             </div>
+
+            <div class="ad-container">
+                <p>
+                    私有部署，请联系: sales@wildfirechat.cn
+                </p>
+            </div>
         </div>
         <footer>
             <p class="proto-version-info">{{ protoRevision() }}</p>
@@ -75,14 +81,6 @@
                 target="_blank">
                 {{ $t('setting.feedback') }}
                 <i class="icon-ion-ios-email-outline"/>
-            </a>
-
-            <a
-                class="button"
-                href="https://github.com/wildfirechat/vue-pc-chat"
-                target="_blank">
-                Star on Github
-                <i class="icon-ion-social-github"/>
             </a>
 
             <a
@@ -281,6 +279,7 @@ export default {
 <style lang="css" scoped>
 .setting-container {
     height: 100%;
+    width: 100%;
     flex: 1;
     display: flex;
     flex-direction: column;
@@ -289,6 +288,7 @@ export default {
 
 .setting-container .content {
     flex: 1;
+    width: 100%;
     margin-left: 20px;
     margin-top: 10px;
 }
@@ -341,6 +341,14 @@ export default {
     text-decoration: none;
     transform: translateY(0px);
     transition: .2s;
+}
+
+.ad-container p {
+    margin-top: 60px;
+    font-size: 30px;
+    text-align: center;
+    color: gray;
+    user-select: text;
 }
 
 .setting-container .button:hover {
