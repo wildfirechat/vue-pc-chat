f869ba53b09d4cabc23b229d96cd37d41511c2a7
x86 <1960867973@qq.com>
2023-04-19 10:33:21 +0800

    更新发布脚本

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,9 +6,15 @@ echo '-------'
 echo ''
 
 cd ..
+echo 'enable ptt'
 sed -i '' 's/ENABLE_PTT = false/ENABLE_PTT = true/' src/config.js
+echo 'enable conference'
 cp src/wfc/av/internal/engine-conference.min.js src/wfc/av/internal/engine.min.js
 
 # 打包所有
 /bin/rm -rf dist_electron
 npm run cross-package-all
+
+echo 'checkout modification'
+git checkout -- src/wfc/av/internal/engine.min.js
+git checkout -- src/config.js
