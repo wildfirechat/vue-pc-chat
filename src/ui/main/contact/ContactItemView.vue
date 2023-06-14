15f10d65ecaa53bf3cac2a86c5880c1ac6068bf8
x86 <1960867973@qq.com>
2023-06-14 11:23:23 +0800

    update style

--- src/ui/main/contact/ContactItemView.vue
+++ src/ui/main/contact/ContactItemView.vue
@@ -55,12 +55,14 @@ export default {
 .label p {
     padding: 5px 5px 5px 0;
     border-bottom: 1px solid #e0e0e0;
+    font-size: 13px;
 }
 
 .contact-item {
     padding: 5px 5px 5px 30px;
     display: flex;
     width: 100%;
+    font-size: 13px;
     align-items: center;
 }
 
