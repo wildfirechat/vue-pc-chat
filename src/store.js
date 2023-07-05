c0223d0f841e6b418986d8b371dc29428d4e744d
x86 <1960867973@qq.com>
2023-07-05 21:52:02 +0800

    修复草稿里面的消息引用丢失问题

--- src/store.js
+++ src/store.js
@@ -1159,6 +1159,7 @@ let store = {
 
     quoteMessage(message) {
         conversationState.quotedMessage = message;
+        conversationState.currentConversationInfo._quotedMessage = message;
     },
 
     getConversationInfo(conversation) {
