8db2add949ee82d760cf9f413800c74fbffdf078
x86 <1960867973@qq.com>
2023-07-06 10:32:48 +0800

    fix #376

--- src/wfc/av/engine/callSession.js
+++ src/wfc/av/engine/callSession.js
@@ -16,7 +16,10 @@ export default class CallSession {
     startTime
     startMsgUid
 
+    callId
+
     // conference 相关
+    pin
     defaultAudience = false;
     audience = false;
     conference = false;
@@ -42,7 +45,7 @@ export default class CallSession {
     /**
      * 播放来电响铃
      */
-    playIncomingRing() {
+    playIncomingRing () {
         // TODO
         //在界面初始化时播放来电铃声
     }
@@ -50,7 +53,7 @@ export default class CallSession {
     /**
      * 停止响铃
      */
-    stopIncomingRing() {
+    stopIncomingRing () {
         // TODO
         //再接听/语音接听/结束媒体时停止播放来电铃声，可能有多次，需要避免出问题
     }
@@ -60,14 +63,14 @@ export default class CallSession {
      * @param {[string]} newParticipantIds
      * @param {boolean}  autoAnswer 是否自动接听，默认 false
      */
-    inviteNewParticipants(newParticipantIds, targetClientId, autoAnswer) {
+    inviteNewParticipants (newParticipantIds, targetClientId, autoAnswer) {
     }
 
     /**
      * 接听来电
      * @deprecated  参考{@link answer}
      */
-    call() {
+    call () {
         this.answer(false, null);
     }
 
@@ -82,12 +85,12 @@ export default class CallSession {
     /**
      * 挂断
      */
-    hangup() {
+    hangup () {
     }
 
 
     // 回落到语音
-    downgrade2Voice() {
+    downgrade2Voice () {
     }
 
     /**
@@ -95,7 +98,7 @@ export default class CallSession {
      * @param enable
      * @deprecated 请使用{@link muteVideo}
      */
-    setVideoEnabled(enable) {
+    setVideoEnabled (enable) {
 
     }
 
@@ -103,7 +106,7 @@ export default class CallSession {
      * 打开或关闭摄像头
      * @param {boolean} mute true，关闭摄像头；false，打开摄像头
      */
-    muteVideo(mute) {
+    muteVideo(mute){
 
     }
 
@@ -112,7 +115,7 @@ export default class CallSession {
      * @param {boolean} enable
      * @deprecated 请使用{@link muteAudio}
      */
-    setAudioEnabled(enable) {
+    setAudioEnabled(enable){
 
     }
 
@@ -120,7 +123,7 @@ export default class CallSession {
      * 静音或取消静音
      * @param {boolean} mute true，静音；false，取消静音
      */
-    muteAudio(mute) {
+    muteAudio(mute){
 
     }
 
@@ -130,7 +133,7 @@ export default class CallSession {
      * @param {[string]} types 媒体源类型，可选screen、window
      * @return {Promise<DesktopCapturerSource[]>}
      */
-    getDesktopSources(types) {
+    getDesktopSources (types) {
 
     }
 
@@ -143,38 +146,39 @@ export default class CallSession {
      *       maxHeight: 720,
      *       idealHeight: 720,
      *       frameRate: 15,
-             }} desktopShareOptions
+             }} desktopShareOptions，sourceId 仅 pc 端有效，web 端无效；其他参数对应 getDisplayMedia(options) options.video，具体可以参考：https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getDisplayMedia
      * 开始屏幕共享
      */
-    async startScreenShare(desktopShareOptions) {
+    async startScreenShare (desktopShareOptions) {
 
     }
 
-    isScreenSharing() {
+    isScreenSharing () {
 
     }
 
-    stopScreenShare() {
+    stopScreenShare () {
 
     }
 
     /**
-     * 请在callState变为connecting 或 connected之后，调用
      * @deprecated
+     * 请在callState变为connecting 或 connected之后，调用
      * @param {string} userId
      * @return {Subscriber}
      */
-    getPeerConnectionClient(userId) {
+    getPeerConnectionClient(userId){
         return this.getSubscriber(userId);
     }
 
     /**
+     * @deprecated 请使用{@link getParticipantProfiles}
      * 请在callState变为connecting 或 connected之后，调用
      * @param {string} userId
      * @param {boolean} screenSharing
      * @return {Subscriber}
      */
-    getSubscriber(userId, screenSharing) {
+    getSubscriber(userId, screenSharing){
     }
 
     /**
@@ -190,6 +194,7 @@ export default class CallSession {
     getSelfProfile(){
 
     }
+
 
     /**
      * 仅会议时有效
@@ -231,7 +236,7 @@ export default class CallSession {
     /**
      * 关闭音视频通话窗口
      */
-    closeVoipWindow() {
+    closeVoipWindow(){
 
     }
 
@@ -240,7 +245,7 @@ export default class CallSession {
      * 设置音频输入设备
      * @param {string} audioDeviceId 音频设备 id
      */
-    setAudioInputDeviceId(audioDeviceId) {
+    setAudioInputDeviceId(audioDeviceId){
 
     }
 
@@ -249,7 +254,7 @@ export default class CallSession {
      * 设置音频输入设备
      * @param {string} videoDeviceId  视频设备 id
      */
-    setVideoInputDeviceId(videoDeviceId) {
+    setVideoInputDeviceId(videoDeviceId){
 
     }
 
@@ -257,7 +262,7 @@ export default class CallSession {
      * 切换摄像头，手机端有效
      * @return true，支持切换摄像头，正在切换；false，不支持切换摄像头
      */
-    switchCamera() {
+    switchCamera(){
 
     }
 
@@ -266,7 +271,7 @@ export default class CallSession {
      * 设置视频最大码率
      * @param maxBitrateKbps
      */
-    setVideoMaxBitrate(maxBitrateKbps) {
+    setVideoMaxBitrate(maxBitrateKbps){
 
     }
 
@@ -276,7 +281,7 @@ export default class CallSession {
      * 相对于原始视频大小按比例缩小分辨率，具体可以参考{@link https://developer.mozilla.org/en-US/docs/Web/API/RTCRtpEncodingParameters/scaleResolutionDownBy}
      * @param {number} scalingFactor
      */
-    scaleVideoResolutionDownBy(scalingFactor) {
+    scaleVideoResolutionDownBy(scalingFactor){
 
     }
 
@@ -285,7 +290,7 @@ export default class CallSession {
      * 请在callState变为connected之后，调用
      * @param {number} ang 旋转角度，可选值为 0，90，180，270
      */
-    rotate(ang) {
+    rotate(ang){
 
     }
 
@@ -296,7 +301,15 @@ export default class CallSession {
      * @param {boolean} isScreenSharing 是否是屏幕共享
      * @param {VideoType} videoType 视频流类型
      */
-    setParticipantVideoType(userId, isScreenSharing, videoType) {
+    setParticipantVideoType(userId, isScreenSharing, videoType){
+
+    }
+
+    /**
+     * 强制关闭媒体流
+     * 正常不需要调用，仅当将通话界面直接渲染在当前界面时，需要调用
+     */
+    forceEndMedia() {
 
     }
 }
