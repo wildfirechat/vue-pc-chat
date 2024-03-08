955d6ecc394686342cbd9866362571cfa68095f1
x86 <1960867973@qq.com>
2024-03-08 16:12:07 +0800

    tmp commit: 切换到vite

---
+++ electron.vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig, externalizeDepsPlugin, bytecodePlugin } from 'electron-vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    main: {
+        plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+    },
+    preload: {
+        plugins: [externalizeDepsPlugin(), bytecodePlugin()]
+    },
+    renderer: {
+        resolve: {
+            alias: {
+                '@renderer': resolve('src/renderer/src')
+            }
+        },
+        plugins: [vue()]
+    }
+})
