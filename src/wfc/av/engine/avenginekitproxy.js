8d3bae0af950470aef2223bb16ebf4577825c122
x86 <1960867973@qq.com>
2024-03-13 09:20:55 +0800

    fix TypeError: Object has been destroyed

--- src/wfc/av/engine/avenginekitproxy.js
+++ src/wfc/av/engine/avenginekitproxy.js
@@ -304,7 +304,12 @@ export class AvEngineKitProxy {
                 // fix object of long.js can be send inter-process
                 args = JSON.stringify(args)
                 if (!this.callWin.isDestroyed()){
-                    this.callWin.webContents.send(event, args);
+                    try {
+                        this.callWin.webContents.send(event, args);
+                    }catch (e) {
+                        // ignore, do nothing
+                        // Object has been destroyed
+                    }
                 }
             } else if (this.queueEvents) {
                 this.queueEvents.push({event, args});
