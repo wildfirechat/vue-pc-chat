48b034fbff20f76584d3978bbb5d5d2c0de9623b
x86 <1960867973@qq.com>
2024-11-29 10:00:17 +0800

    Revert "支持剪贴板图片同步"
    This reverts commit 3da130aa988c4d8f3b495895ffb303dfbf845a11.

--- src/platform.js
+++ src/platform.js
@@ -31,7 +31,6 @@ export const AppPath = require('@electron/remote').app.getAppPath();
 export const app = require('@electron/remote').app;
 export const screen = require('@electron/remote').screen
 export const clipboard = require('electron').clipboard;
-export const nativeImage = require('electron').nativeImage;
 
 // for web
 
