a395bd3ba4622e93e4bb2420ed31f80edeeb16a3
x86 <1960867973@qq.com>
2024-12-17 23:24:04 +0800

    添加是否在单人音视频通话页面显示音视频 SDK 相关提示配置

--- src/ui/voip/Single.vue
+++ src/ui/voip/Single.vue
@@ -9,11 +9,11 @@
     <div class="flex-column flex-align-center flex-justify-center" style="background: #292929">
         <h1 style="display: none">Voip-single，运行在新的window，和主窗口数据是隔离的！！</h1>
 
-        <p class="webrtc-tip" v-if="showVoipTip">
+        <div class="webrtc-tip" v-if="showVoipTip">
             <p>{{ supportConference ? '当前使用：多人版音视频' : '当前使用：高级版音视频' }}</p>
             <p>多人版音视频 和 高级版音视频不互通，切换方法请参考: wfc/av/internal/README.MD</p>
             <p>{{ voipTip }}</p>
-        </p>
+        </div>
         <div v-if="session" class="container">
             <section class="full-height full-width">
                 <!--audio-->
@@ -149,7 +149,7 @@ export default {
             audioInputDeviceIndex: 0,
             autoPlayInterval: 0,
             ringAudio: null,
-            showVoipTip: true,
+            showVoipTip: Config.SHOW_VOIP_TIP,
             voipTip: '',
             supportConference: avenginekit.startConference !== undefined,
         }
