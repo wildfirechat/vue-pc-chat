ad786ccaecdbade9ea94909f0f9e5be20f86d1ec
x86 <1960867973@qq.com>
2026-03-20 14:47:06 +0800

    组织结构服务不可用时，选人界面，不显示从组织结构选取选项

--- src/api/organizationServerApi.js
+++ src/api/organizationServerApi.js
@@ -6,7 +6,7 @@ import OrganizationServerError from "./organizationServerError";
 import UserInfo from "../wfc/model/userInfo";
 
 export class OrganizationServerApi {
-    isServiceAvailable = true;
+    isServiceAvailable = false;
     serviceUnavailbelError = new OrganizationServerError(-1, '未登录或服务不可用');
 
     constructor() {
@@ -138,6 +138,10 @@ export class OrganizationServerApi {
      * @private
      */
     async _post(path, data = {}, rawResponse = false, rawResponseData = false) {
+        if(!this.isServiceAvailable){
+            throw new Error("service not available");
+        }
+
         let response;
         path = Config.ORGANIZATION_SERVER + path;
         response = await axios.post(path, data, {
