5b24b0ff46075350bf2b2074e9da937571636b57
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2026-04-13 18:53:57 +0800

    uos and kylin app store
    # Conflicts:
    #	src/wfc/proto/proto.min.js

--- entries/install.sh
+++ entries/install.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-desktopPath=/opt/野火IM/entries/applications/cn.wildfire.chat.desktop
+desktopPath=/opt/cn.wildfirechat.pcclient/files/entries/applications/cn.wildfirechat.pcclient.desktop
 rootDesktop=/root/Desktop
 if [ -d $rootDesktop ]; then
   cp $desktopPath $rootDesktop
@@ -11,10 +11,10 @@ for u in $users; do
   cndir=/home/$u/桌面
   if [ -d $dir ]; then
     cp $desktopPath $dir
-    chmod 777 $dir/cn.wildfire.chat.desktop
+    chmod 777 $dir/cn.wildfirechat.pcclient.desktop
   fi
   if [ -d $cndir ]; then
     cp $desktopPath $cndir
-    chmod 777 $cndir/cn.wildfire.chat.desktop
+    chmod 777 $cndir/cn.wildfirechat.pcclient.desktop
   fi
 done
